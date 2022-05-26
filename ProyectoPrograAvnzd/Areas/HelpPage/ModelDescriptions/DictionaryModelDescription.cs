d872bb2ae933b2645f6afbdeab7e0018d042535f
Darío Monestel Montero <73733002+monesteldario@users.noreply.github.com>
2022-05-26 01:06:22 -0600

    Add files via upload

---
+++ ProyectoPrograAvnzd/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProyectoPrograAvnzd.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ProyectoPrograAvnzd/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProyectoPrograAvnzd.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
